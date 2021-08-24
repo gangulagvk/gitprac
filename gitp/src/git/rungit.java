939f1119f57e86cc87c766d0f0d31f3c0f341df1
vamsikumar <vamsikumar@GGVK>
2021-08-24 15:50:29 +0530

    op

---
+++ gitp/src/git/rungit.java
@@ -0,0 +1,15 @@
+package git;
+
+public class rungit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=50;
+		int b=60;
+		int t;
+		t=a+b;
+		System.out.println(t);
+
+	}
+
+}
